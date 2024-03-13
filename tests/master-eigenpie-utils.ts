d0d8d205fe2856013435cf8c9b34d6d1d2085b83
Gajendra <gajendra@magpiexyz.io>
2024-03-13 20:49:53 +0530

    Add Test cases

--- tests/master-eigenpie-utils.ts
+++ tests/master-eigenpie-utils.ts
@@ -2,39 +2,47 @@ import { newMockEvent } from "matchstick-as";
 import { ethereum, Address, BigInt } from "@graphprotocol/graph-ts";
 import { TokenExchange as CurveLpTokenExchangeEvent } from "../generated/CurveLP/CurveLP";
 export function createCurveLpTokenExchangeEvent(
-  depositor: Address,
-  address: Address,
-  depositAmount: BigInt,
-  poinboost: BigInt,
-  referral: Address
+  id: i32,
+  buyer: string,
+  sold_id: BigInt,
+  tokens_sold: BigInt,
+  bought_id: BigInt,
+  tokens_bought: BigInt
 ): CurveLpTokenExchangeEvent {
   let curveLpTokenExchangeEvent = changetype<CurveLpTokenExchangeEvent>(
     newMockEvent()
   );
 
   curveLpTokenExchangeEvent.parameters = new Array();
+  let idParam = new ethereum.EventParam("id", ethereum.Value.fromI32(id));
+  let buyerParam = new ethereum.EventParam(
+    "buyer",
+    ethereum.Value.fromAddress(Address.fromString(buyer))
+  );
+  let soldIdParam = new ethereum.EventParam(
+    "sold_id",
+    ethereum.Value.fromUnsignedBigInt(sold_id)
+  );
+  let tokenSoldParam = new ethereum.EventParam(
+    "tokens_sold",
+    ethereum.Value.fromUnsignedBigInt(tokens_sold)
+  );
+  let boughtIdParam = new ethereum.EventParam(
+    "bought_id",
+    ethereum.Value.fromUnsignedBigInt(bought_id)
+  );
+  let tokensBoughtParam = new ethereum.EventParam(
+    "tokens_bought",
+    ethereum.Value.fromUnsignedBigInt(tokens_bought)
+  );
+  // check whether the timestamp event is made or not?
 
-  curveLpTokenExchangeEvent.parameters.push(
-    new ethereum.EventParam("depositor", ethereum.Value.fromAddress(depositor))
-  );
-  curveLpTokenExchangeEvent.parameters.push(
-    new ethereum.EventParam("address", ethereum.Value.fromAddress(address))
-  );
-  curveLpTokenExchangeEvent.parameters.push(
-    new ethereum.EventParam(
-      "depositAmount",
-      ethereum.Value.fromUnsignedBigInt(depositAmount)
-    )
-  );
-  curveLpTokenExchangeEvent.parameters.push(
-    new ethereum.EventParam(
-      "poinboost",
-      ethereum.Value.fromUnsignedBigInt(poinboost)
-    )
-  );
-  curveLpTokenExchangeEvent.parameters.push(
-    new ethereum.EventParam("referral", ethereum.Value.fromAddress(referral))
-  );
+  curveLpTokenExchangeEvent.parameters.push(idParam);
+  curveLpTokenExchangeEvent.parameters.push(buyerParam);
+  curveLpTokenExchangeEvent.parameters.push(soldIdParam);
+  curveLpTokenExchangeEvent.parameters.push(tokenSoldParam);
+  curveLpTokenExchangeEvent.parameters.push(boughtIdParam);
+  curveLpTokenExchangeEvent.parameters.push(tokensBoughtParam);
 
   return curveLpTokenExchangeEvent;
 }
--- tests/masterEigenpie.test.ts
+++ tests/masterEigenpie.test.ts
@@ -1,6 +1,15 @@
 import { Address, BigInt, Bytes } from "@graphprotocol/graph-ts";
 import { beforeAll, describe, newMockEvent, test } from "matchstick-as";
-import { assert, clearStore, afterAll } from "matchstick-as/assembly/index";
+import {
+  assert,
+  createMockedFunction,
+  clearStore,
+  newMockCall,
+  countEntities,
+  mockIpfsFile,
+  afterEach,
+  afterAll,
+} from "matchstick-as/assembly/index";
 import {
   Transfer as CurveLpTransferEvent,
   TokenExchange as CurveLpTokenExchangeEvent,
@@ -13,7 +22,31 @@ import {
   TokenExchange,
 } from "../generated/CurveLP/CurveLP";
 import { createCurveLpTokenExchangeEvent } from "./master-eigenpie-utils";
-
+import {
+  GlobalInfo,
+  GroupInfo,
+  LpInfo,
+  PoolInfo,
+  UserBalanceInfo,
+  UserInfo,
+} from "../generated/schema";
+import {
+  ADDRESS_ZERO,
+  BIGINT_ONE,
+  BIGINT_TWO,
+  BIGINT_ZERO,
+  DENOMINATOR,
+  EIGENPIE_PREDEPLOST_HELPER,
+  EIGEN_LAYER_LAUNCH_TIME,
+  EIGEN_LAYER_POINT_PER_SEC,
+  ETHER_ONE,
+  ETHER_TEN,
+  LPTOKEN_LIST,
+  LST_PRICE_MAP,
+  LST_TO_MLRT_MAP,
+  MSTETH_WSTETH_CURVE_LP,
+  MSWETH_SWETH_CURVE_LP,
+} from "../src/constants";
 // import { Block, Transaction } from "@graphprotocol/graph-ts";
 import {
   handleCurveLpTransfer,
@@ -28,61 +61,130 @@ import {
   handlePriceProviderExchangeRateUpdateEvent,
 } from "../src/masterEigenpie";
 
-describe("MasterEigenpie", () => {
-  test("Test Deposit", () => {});
+test(
+  "Should throw an error",
+  () => {
+    throw new Error();
+  },
+  true
+);
+
+describe("Entity Store For LpInfo", () => {
   beforeAll(() => {
-    let depositor = Address.fromString(
-      "0x0000000000000000000000000000000000000001"
+    let lpInfo = new LpInfo(Bytes.fromHexString("lpInfoId0"));
+    lpInfo.lpToken = Bytes.fromHexString("lpInfoId0");
+    lpInfo.priceToETH = ETHER_ONE;
+    lpInfo.mLrtRatio = ETHER_ONE;
+    lpInfo.eigenLayerPointsPerSec = EIGEN_LAYER_POINT_PER_SEC;
+    lpInfo.eigenpiePointsPerSec = BigInt.fromString("1000000000000000000").div(
+      BigInt.fromI32(3600)
     );
-    let address = Address.fromString(
-      "0x2022d9AF896eCF0F1f5B48cdDaB9e74b5aAbCf00"
-    );
-    let depositAmount = BigInt.fromI32(234);
-    let poinboost = BigInt.fromI32(234);
-    let referral = Address.fromString(
-      "0x0000000000000000000000000000000000000001"
-    );
-    let newCurveLpTokenExchangeEvent = createCurveLpTokenExchangeEvent(
-      depositor,
-      address,
-      depositAmount,
-      poinboost,
-      referral
-    );
-    handleCurveTrading(newCurveLpTokenExchangeEvent);
+    lpInfo.save();
   });
 
   afterAll(() => {
     clearStore();
   });
 
-  test("AssetDeposit created and stored", () => {
+  test("Can use entity.loadInBlock() to retrieve entity from cache store in the current block", () => {
+    let retrievedLpInfo = LpInfo.loadInBlock(Bytes.fromHexString("LpInfo0"));
+    assert.bytesEquals(
+      Bytes.fromHexString("LpInfo0"),
+      retrievedLpInfo!.get("id")!.toBytes()
+    );
+  });
+
+  test("Returns null when calling entity.loadInBlock() if an entity doesn't exist in the current block", () => {
+    let retrievedLpInfo = LpInfo.loadInBlock(
+      Bytes.fromHexString("IDoNotExist")
+    );
+    assert.assertNull(retrievedLpInfo);
+  });
+
+  test("Can use entity.load() to retrieve entity from store", () => {
+    let retrievedLpInfo = LpInfo.load(Bytes.fromHexString("LpInfo0"));
+    assert.bytesEquals(
+      Bytes.fromHexString("LpInfo0"),
+      retrievedLpInfo!.get("id")!.toBytes()
+    );
+  });
+
+  test("Returns null when calling entity.load() if an entity doesn't exist", () => {
+    let retrievedLpInfo = LpInfo.load(Bytes.fromHexString("IDoNotExist"));
+    assert.assertNull(retrievedLpInfo);
+  });
+
+  test("Can update entity that already exists using Entity.save()", () => {
+    // Retrieve same entity from the store
+    let lpinfo = LpInfo.load(Bytes.fromHexString("LpInfo0"));
+    if (lpinfo != null) {
+      lpinfo.set("priceToETH", ETHER_ONE.plus(ETHER_ONE)); // check why error throwing
+      lpinfo.save();
+
+      assert.fieldEquals(
+        "LpInfo",
+        "LpInfo0",
+        "priceToETH",
+        "2000000000000000000"
+      );
+    }
+  });
+
+  test("Can assert amount of entities of a certain type in store", () => {
+    clearStore();
+    assert.entityCount("LpInfo", 0);
+
+    let counter = 1;
+    while (countEntities("LpInfo") < 2) {
+      let newLpInfo = new LpInfo(
+        Bytes.fromHexString("id" + counter.toString())
+      );
+      newLpInfo.priceToETH = ETHER_ONE;
+      newLpInfo.mLrtRatio = ETHER_ONE;
+      newLpInfo.eigenLayerPointsPerSec = EIGEN_LAYER_POINT_PER_SEC;
+      newLpInfo.eigenpiePointsPerSec = BigInt.fromString(
+        "1000000000000000000"
+      ).div(BigInt.fromI32(3600));
+      newLpInfo.save();
+      counter++;
+    }
+
+    assert.entityCount("LpInfo", 2);
+  });
+});
+
+describe("Mocked Events", () => {
+  afterEach(() => {
+    clearStore();
+  });
+
+  test("Can call mappings with custom events", () => {
+    // Call mappings
+    let newCurveLpTokenExchangeEvent = createCurveLpTokenExchangeEvent(
+      0xdead,
+      "0x53e12dfA22a903dCC0c54aBAf0E5017eEfC9DaF6",
+      BigInt.fromI32(0),
+      BigInt.fromString("63993754201653557777"),
+      BigInt.fromI32(1),
+      BigInt.fromString("54493385790308612876")
+    );
+
+    handleCurveTrading(newCurveLpTokenExchangeEvent);
+
     assert.entityCount("LpInfo", 1);
-
-    let defaultAddress = "0xa16081f360e3847006db660bae1c6d1b2e17ec2a";
-    // 0xa16081f360e3847006db660bae1c6d1b2e17ec2a is the default address used in newMockEvent() function
-    // assert.fieldEquals(
-    //   "LpInfo",
-    //   defaultAddress,
-    //   "depositor",
-    //   "0x0000000000000000000000000000000000000001"
-    // );
-    // assert.fieldEquals(
-    //   "LpInfo",
-    //   defaultAddress,
-    //   "asset",
-    //   "0x0000000000000000000000000000000000000001"
-    // );
-    // assert.fieldEquals("LpInfo", defaultAddress, "depositAmount", "234");
-    // assert.fieldEquals("LpInfo", defaultAddress, "poinboost", "234");
-    // assert.fieldEquals(
-    //   "LpInfo",
-    //   defaultAddress,
-    //   "referral",
-    //   "0x0000000000000000000000000000000000000001"
-    // );
-
-    //   // More assert options:
-    //   // https://thegraph.com/docs/en/developer/matchstick/#asserts
+    assert.fieldEquals("LpInfo", "0xdead", "sold_id", "0");
+    assert.fieldEquals("LpInfo", "0xdead", "bought_id", "1");
+    assert.fieldEquals(
+      "LpInfo",
+      "0xdead",
+      "tokens_sold",
+      "63993754201653557777"
+    );
+    assert.fieldEquals(
+      "LpInfo",
+      "0xdead",
+      "tokens_bought",
+      "54493385790308612876"
+    );
   });
 });
